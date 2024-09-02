2498bd231df99c9e244fd74a9dc1e9c1d48cfa11
Dhruv Desai <120179026+dhruvdesai09@users.noreply.github.com>
2024-09-02 13:00:24 +0530

    integrated flask

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from flask import Flask, request, jsonify
 from cleantext import preprocess_text
 from train_categorizer import categorize_complaint
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
-sample_text = ""
-clean_text = preprocess_text(sample_text)
-category, sub_category = categorize_complaint(clean_text)
-print(f"Category:{category} \nSub-category:{sub_category}")
+@app.route('/categorize', methods=['POST'])
+def categorize():
+    data = request.json
+    sample_text = data.get('text', '')
+    clean_text = preprocess_text(sample_text)
+    category, sub_category = categorize_complaint(clean_text)
+    return jsonify({'category': category, 'subCategory': sub_category})
+
+if __name__ == '__main__':
+    app.run(debug=True)
